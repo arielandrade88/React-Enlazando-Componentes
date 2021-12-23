a24dbe46b2f0326380be4b31dc0f87f7ff6f6b04
arielhandrade88 <arielhandrade@hotmail.com.ar>
2021-12-23 00:31:28 -0300

    Enlazando Componentes

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import SideBar from './SideBar';
+import { Switch, Route } from 'react-router-dom'
+import ContentWrapper from './ContentWrapper';
+import LastMovieInDb from './LastMovieInDb';
+import GenresInDb from './GenresInDb';
+import ContentRowMovies from './ContentRowMovies';
+import NoEncontreNada from './NotFound';
+function App() {
+  return (
+    <React.Fragment>
+      	<div id="wrapper">
+          <SideBar />
+          <Switch>
+            <Route path="/" exact>
+                <ContentWrapper />
+            </Route>
+            <Route path="/LastMovieInDb"  component={LastMovieInDb}/>
+            <Route path="/GenresInDb"  component={GenresInDb}/>
+            <Route path="/ContentRowMovies"  component={ContentRowMovies}/>
+            <Route component={NoEncontreNada} />
+          </Switch>
+        </div>
+    </React.Fragment>
+     
+   
+
+  );
+}
+
+export default App;
+
+
